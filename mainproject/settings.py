775d7331ea32b9ccfae2c5622e8decf2de92d09c
mcemorales <mcemorales21@gmail.com>
2019-11-16 15:27:05 -0800

    Sixth

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -21,11 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vu7(f_qx_g5)yqn+f)#f)uv85=pti#bb#l45m&nekhsf8dti^!'
+# SECRET_KEY = 'vu7(f_qx_g5)yqn+f)#f)uv85=pti#bb#l45m&nekhsf8dti^!'
+
+SECRET_KEY = os.environ.get('BLOG_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -151,7 +153,7 @@ MESSAGE_TAGS = {
 LOGIN_REDIRECT_URL = 'reservation'
 LOGOUT_REDIRECT_URL = '/'
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
